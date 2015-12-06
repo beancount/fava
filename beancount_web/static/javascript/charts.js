5a4e31205c76455e09f900fddabd122b926e324a
Dominik Aumayr <dominik@aumayr.name>
2015-12-06 20:53:23 +0100

    Fix chart toggle

--- beancount_web/static/javascript/charts.js
+++ beancount_web/static/javascript/charts.js
@@ -56,67 +56,10 @@ $(document).ready(function() {
         }
     });
 
-    // if (isset("data_account")) {
-    //     new Chartist.Line('#chart-account', {
-    //         series: [{
-    //             name: 'remaining',
-    //             data: data_account
-    //         }]
-    //     }, { // options
-    //         axisX: {
-    //             type: Chartist.FixedScaleAxis,
-    //             divisor: data_account.length > 15 ? 15 : data_account.length,  // no of days
-    //             labelInterpolationFnc: function(value) {
-    //                 return moment(value).format('MMM D');
-    //             }
-    //         },
-    //         axisY: {
-    //             onlyInteger: true,
-    //             // low: 0,
-    //             position: 'end',
-    //             labelInterpolationFnc: function(value) {
-    //                 return value.toLocaleString();
-    //             }
-    //         },
-    //         series: {
-    //             remaining: {
-    //                 // lineSmooth: Chartist.Interpolation.step(),
-    //                 // showPoint: true
-    //             },
-    //         },
-    //         height: 240,
-    //         width: $('.charts').width(),
-    //         chartPadding: { left: 3 }
-    //     });
-    // }
-
-    // if (isset("data_monthly_totals")) {
-        // new Chartist.Bar('#chart-monthly-totals', data_monthly_totals, {
-        //     height: 240,
-        //     width: $('.charts').width(),
-        //     chartPadding: { left: 0, right: 0 },
-        //     axisY: {
-        //         onlyInteger: true,
-        //         referenceValue: 0,
-        //         position: 'end',
-        //         scaleMinSpace: 15,
-        //         labelInterpolationFnc: function(value) {
-        //             return value.toLocaleString();
-        //         }
-        //     },
-        //     axisX: {
-        //         divisor: data_monthly_totals.length > 15 ? 15 : data_monthly_totals.length,  // no of days
-        //         labelInterpolationFnc: function(value, index) {
-        //             return moment(value).format("MMM 'YY");
-        //         }
-        //     }
-        // });
-    // }
-
     // Toggle multiple charts
     $('.charts .chart-labels label').click(function() {
-        $('.charts .ct-chart').hide();
-        $('.charts .ct-chart#' + $(this).prop('for')).show();
+        $('.charts .ct-chart').addClass('hidden')
+        $('.charts .ct-chart#' + $(this).prop('for')).removeClass('hidden');
 
         $('.charts .chart-labels label').removeClass('selected');
         $(this).addClass('selected');
@@ -125,7 +68,7 @@ $(document).ready(function() {
 
     // Toggle chart by clicking on "Hide/Show chart"
     $('.charts input#toggle-chart').click(function() {
-        $('.ct-chart, .chart-labels').not('.hidden').toggle($(this).hasClass('hide-charts'));
+        $('.charts-container, .chart-labels').toggle($(this).hasClass('hide-charts'));
         $(this).toggleClass('hide-charts');
 
         var text = $(this).prop('value');
