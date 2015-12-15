d9c0f043249bc23f6e9b8218fe2a2d54ecc034c0
Dominik Aumayr <dominik@aumayr.name>
2015-12-15 08:22:24 +0100

    Add beancount_web.util to packages (fixes #37), bump version to 0.1.0a2

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(name='beancount-web',
-      version='0.1.0a1',
+      version='0.1.0a2',
       description='A web interface for beancount',
       url='http://github.com/aumayr/beancount-web',
       author='Dominik Aumayr',
       author_email='dominik@aumayr.name',
       license='MIT',
-      packages=['beancount_web'],
+      packages=['beancount_web',
+                'beancount_web.util'],
       entry_points={
           'console_scripts': [
               'beancount-web = beancount_web.cli:main',
