3b196a819ee0c8dec20bc64b3c01220643260ce7
Dominik Aumayr <dominik@aumayr.name>
2016-02-15 18:04:33 +0100

    Bumped version number

--- fava/__init__.py
+++ fava/__init__.py
@@ -14,7 +14,7 @@
 from __future__ import absolute_import, unicode_literals, print_function
 from .util.version import get_version
 
-VERSION = (0, 2, 2, '', 0)
+VERSION = (0, 2, 3, '', 0)
 
 __url__             = "http://github.com/aumayr/fava"
 __version__         = get_version(VERSION)
