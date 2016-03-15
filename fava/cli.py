4e0f5c0c6662c710a77485bccc8e6f95569ea670
Jakob Schnitzer <mail@jakobschnitzer.de>
2016-03-15 15:47:19 +0100

    use click for cli

--- fava/cli.py
+++ fava/cli.py
@@ -1,109 +1,68 @@
 # -*- coding: utf-8 -*-
-import argparse
 import os
-import sys
 import errno
 
+import click
 from livereload import Server
 
 from fava.application import app, load_settings
 
 
-def run(argv):
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
+@click.command()
+@click.argument('filename', type=click.Path(exists=True))
+@click.option('-p', '--port', type=int, default=5000,
+              help='The port to listen on. (default: 5000)')
+@click.option('-H', '--host', type=str, default='localhost',
+              help='The host to listen on. (default: localhost)')
+@click.option('-s', '--settings',
+              type=click.Path(exists=True, resolve_path=True),
+              help='Settings file for fava.')
+@click.option('-d', '--debug', is_flag=True,
+              help='Turn on debugging. Disables live-reloading.')
+@click.option('--profile', is_flag=True,
+              help='Turn on profiling. Implies --debug.')
+@click.option('--profile-dir', type=click.Path(),
+              help='Output directory for profiling data.')
+@click.option('--profile-restriction', type=int, default=30,
+              help='Number of functions to show in profile.')
+def main(filename, port, host, settings, debug, profile, profile_dir,
+         profile_restriction):
+    """Start fava for FILENAME on http://host:port."""
 
-    parser.add_argument('-p', '--port',
-                        action='store',
-                        type=int,
-                        default=5000,
-                        help="Port to listen on.")
+    if profile_dir:
+        profile = True
+    if profile:
+        debug = True
 
-    parser.add_argument('-H', '--host',
-                        action='store',
-                        type=str,
-                        default='localhost',
-                        help="Host for the webserver.")
+    app.beancount_file = filename
 
-    parser.add_argument('-s', '--settings',
-                        action='store',
-                        type=str,
-                        help="Settings-file for fava.")
-
-    parser.add_argument('-d', '--debug',
-                        action='store_true',
-                        help="Turn on debugging. This uses the built-in Flask"
-                             "webserver, and live-reloading of beancount-files"
-                             "is disabled.")
-
-    parser.add_argument('--profile',
-                        action='store_true',
-                        help="Turn on profiling.  Implies --debug.  Profiling"
-                             "information for each request will be printed to"
-                             "the log, unless --pstats-output is also"
-                             "specified.")
-
-    parser.add_argument('--pstats-output',
-                        type=str,
-                        help="Output directory for profiling pstats data.  \
-                              Implies --profile.  If this is specified, \
-                              profiling information will be saved to the \
-                              specified directly and will not be printed to \
-                              the log.")
-
-    parser.add_argument('--profile-restriction',
-                        type=int,
-                        default=30,
-                        help='Maximum number of functions to show in profile printed \
-                              to the log.')
-
-    parser.add_argument('filename',
-                        type=str,
-                        help="Beancount input file.")
-
-    args = parser.parse_args(argv)
-
-    if args.pstats_output is not None:
-        args.profile = True
-    if args.profile:
-        args.debug = True
-
-    app.beancount_file = args.filename
-
-    if args.debug:
+    if debug:
         app.api.load_file(app.beancount_file)
-        if args.settings:
-            load_settings(os.path.realpath(args.settings))
+        if settings:
+            load_settings(settings)
 
-        if args.profile:
+        if profile:
             from werkzeug.contrib.profiler import ProfilerMiddleware
             app.config['PROFILE'] = True
-            kwargs = {}
-            if args.pstats_output is not None:
-                kwargs['profile_dir'] = args.pstats_output
             app.wsgi_app = ProfilerMiddleware(
                 app.wsgi_app,
-                restrictions=[args.profile_restriction], **kwargs)
+                restrictions=(profile_restriction),
+                profile_dir=profile_dir if profile_dir else None)
 
-        app.config['ASSETS_CACHE'] = True
-        app.config['ASSETS_DEBUG'] = True
-
-        app.run(args.host, args.port, args.debug)
+        app.run(host, port, debug)
     else:
         server = Server(app.wsgi_app)
         reload_source_files(server)
-        if args.settings:
-            reload_settings(server, os.path.realpath(args.settings))
+        if settings:
+            reload_settings(server, settings)
 
         try:
-            server.serve(port=args.port, host=args.host, debug=args.debug)
+            server.serve(port=port, host=host, debug=debug)
         except OSError as e:
             if e.errno == errno.EADDRINUSE:
                 print("Error: Can not start webserver because the port/address"
                       "is already in use.")
-                print("Please choose another port with the '-p' option."
-                      "({})".format(e))
+                print("Please choose another port with the '-p' option.")
             else:
                 raise
         except:
@@ -126,10 +85,3 @@ def reload_settings(server, settings_path):
     """Auto-reload the settings-file."""
     load_settings(settings_path)
     server.watch(settings_path, lambda: reload_settings(server, settings_path))
-
-
-def main():
-    run(sys.argv[1:])
-
-if __name__ == '__main__':
-    main()
--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     },
     install_requires=[
         'beancount>=2.0b6',
+        'click',
         'pygments>=2.1.1',
         'beancount-pygments-lexer>=0.1.2',
         'markdown2>=2.3.0',
