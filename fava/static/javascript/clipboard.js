b2ffc5e9e168181a1cdd7cf0df33b9c2d30d2043
Dominik Aumayr <dominik@aumayr.name>
2016-03-23 08:40:25 +0100

    Fix clipboard copying when text is very long (#221)

--- fava/static/javascript/clipboard.js
+++ fava/static/javascript/clipboard.js
@@ -4,7 +4,7 @@ module.exports.initClipboard = function() {
     new Clipboard('.status-indicator');
     new Clipboard('#copy-balances', {
         text: function(trigger) {
-            var text = trigger.getAttribute('title').split('\n');
+            var text = trigger.getAttribute('data-clipboard-text').split('\n');
             text.splice(0, 2);
             return text.join('\n').trim();
         }
