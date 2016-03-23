b1d21b3d6ecad458151eaaed40166e9daa063470
Dominik Aumayr <dominik@aumayr.name>
2016-03-23 08:04:27 +0100

    Fix script for fava.pythonanywhere.com
    fix #237

--- contrib/pythonanywhere/fava/fava_pythonanywhere_com_wsgi.py
+++ contrib/pythonanywhere/fava/fava_pythonanywhere_com_wsgi.py
@@ -1,14 +1,8 @@
-import configparser
-from fava.api import BeancountReportAPI
 from fava.application import app as application
+from fava.application import load_file, load_settings
 
-application.beancount_file = '/home/fava/test.bean'
-application.filter_year = None
-application.filter_tag = None
-application.api = BeancountReportAPI(application.beancount_file)
+application.config['BEANCOUNT_FILE'] = '/home/fava/test.bean'
+application.config['USER_SETTINGS'] = None
 
-application.user_config = configparser.ConfigParser()
-user_config_defaults_file = '/home/fava/default-settings.conf'
-application.user_config.readfp(open(user_config_defaults_file))
-application.user_config['fava']['file_defaults'] = user_config_defaults_file
-application.user_config['fava']['file_user'] = ''
+load_settings()
+load_file()
