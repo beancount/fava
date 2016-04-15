ecea5e066d40a0ccd16ced947032226b57c29079
Dominik Aumayr <dominik@aumayr.name>
2016-04-15 08:36:04 +0200

    Parse Journal filters from get params
    fix #239

--- fava/static/javascript/journal.js
+++ fava/static/javascript/journal.js
@@ -1,3 +1,5 @@
+const queryString = require('query-string');
+
 module.exports.initJournal = function() {
     // Toggle legs by clicking on transaction/padding row
     $('#journal-table li.transaction').click(function() {
@@ -8,14 +10,27 @@ module.exports.initJournal = function() {
     $('#entry-filters input').click(function() {
         event.preventDefault();
         var $this = $(this);
-        var type = $this.attr('data-type');
+        var selector = $this.attr('data-selector');
         var shouldShow = $this.hasClass('inactive');
 
         if ($this.val() == 'Transaction') {
             $('#entry-filters .txn-toggle').toggleClass('inactive', !shouldShow);
         }
 
-        $('#journal-table ' + type).toggleClass('hidden', !shouldShow);
+        $('#journal-table ' + selector).toggleClass('hidden', !shouldShow);
         $this.toggleClass('inactive', !shouldShow);
+
+        // Modify get params
+        const queryHash = queryString.parse(location.search);
+        var type = $this.attr('data-type');
+        var defaultShow = $this.attr('data-show-default') === 'true';
+
+        if (shouldShow !== defaultShow) {
+            queryHash[type] = shouldShow;
+        } else if (type in queryHash) {
+            delete queryHash[type];
+        }
+
+        window.history.pushState('', '', '?' + queryString.stringify(queryHash));
     });
 }
