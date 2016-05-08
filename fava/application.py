da58ad2ba07bf2b3aa628e42a28356f927504598
Dominik Aumayr <dominik@aumayr.name>
2016-05-08 21:44:34 +0200

    Fix file downloads/uploads

--- fava/application.py
+++ fava/application.py
@@ -108,12 +108,11 @@ def account_with_interval_changes(name):
 @app.route('/<bfile>/document/', methods=['GET'])
 def document():
     document_path = request.args.get('file_path', None)
-
     if document_path and g.api.is_valid_document(document_path):
         # metadata-statement-paths may be relative to the beancount-file
         if not os.path.isabs(document_path):
             document_path = os.path.join(os.path.dirname(
-                os.path.realpath(app.config['BEANCOUNT_FILE'])), document_path)
+                os.path.realpath(g.api.beancount_file_path)), document_path)
 
         directory = os.path.dirname(document_path)
         filename = os.path.basename(document_path)
@@ -126,16 +125,11 @@ def document():
 def add_document():
     file = request.files['file']
     if file and len(g.api.options['documents']) > 0:
-        # and allowed_file(file.filename):
-        # TOOD Probably it should ask to enter a date, if the document
-        #      doesn't start with one, so you don't need to rename the
-        #      documents in advance.
-
         target_folder_index = int(request.form['targetFolderIndex'])
         target_folder = g.api.options['documents'][target_folder_index]
 
         filename = os.path.join(
-            os.path.dirname(app.config['BEANCOUNT_FILE']),
+            os.path.dirname(g.api.beancount_file_path),
             target_folder,
             request.form['account_name'].replace(':', '/').replace('..', ''),
             secure_filename(request.form['filename']))
