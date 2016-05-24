08b613b4960c5ddf0a3e72547ae187ebc06199da
Dominik Aumayr <dominik@aumayr.name>
2016-05-24 10:56:49 +0200

    Update scripts for fava.pythonanywere.com

--- contrib/pythonanywhere/fava/fava_pythonanywhere_com_wsgi.py
+++ contrib/pythonanywhere/fava/fava_pythonanywhere_com_wsgi.py
@@ -1,8 +1,14 @@
 from fava.application import app as application
 from fava.application import load_file, load_settings
 
-application.config['BEANCOUNT_FILE'] = '/home/fava/test.bean'
+application.config['BEANCOUNT_FILES'] = ['/home/fava/test1.bean', '/home/fava/test2.bean']
 application.config['USER_SETTINGS'] = None
 
 load_settings()
 load_file()
+
+# app.user_config = configparser.ConfigParser()
+# user_config_defaults_file = '/home/fava/default-settings.conf'
+# app.user_config.readfp(open(user_config_defaults_file))
+# app.user_config['fava']['file_defaults'] = user_config_defaults_file
+# app.user_config['fava']['file_user'] = ''
--- contrib/pythonanywhere/fava/update.sh
+++ contrib/pythonanywhere/fava/update.sh
@@ -4,7 +4,7 @@
 export PATH=$PATH:/home/fava/.local/bin
 
 # Updade fava
-pip3 install --user beancount-fava
+pip3 install --user beancount-fava --upgrade
 version=`python3 -c "import fava; print(fava.__version__)"`
 
 # Update default config
@@ -14,7 +14,9 @@ cp ~/fava/fava/default-settings.conf /home/fava/
 date=`date +%Y-%m-%d`
 site='PyPI'
 name="option \"title\" \"Example beancount-fava @ $version ($date) [$site]\""
-bean-example | sed "7s#.*#$name#" > test.bean
+bean-example | sed "7s#.*#$name#" > test1.bean
+name="option \"title\" \"Example (2) beancount-fava @ $version ($date) [$site]\""
+bean-example | sed "7s#.*#$name#" > test2.bean
 
 # Reload web page
 touch /var/www/fava_pythonanywhere_com_wsgi.py
