50b5ceb9c5f858184fe5e55a736320ef34e66870
Jakob Schnitzer <mail@jakobschnitzer.de>
2016-06-10 11:44:27 +0200

    JS: use babel to transpile
    We're using some ES6 features already and our ESLint encourages to use
    even more of them, so let's do it.

--- fava/static/webpack.config.js
+++ fava/static/webpack.config.js
@@ -1,47 +1,51 @@
-var webpack = require('webpack');
-var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var extractThemeDefaultCSS     = new ExtractTextPlugin('theme_default.css',     { allChunks: true });
-var extractThemeAlternativeCSS = new ExtractTextPlugin('theme_alternative.css', { allChunks: true });
+const extractDefault = new ExtractTextPlugin('theme_default.css', { allChunks: true });
+const extractAlternative = new ExtractTextPlugin('theme_alternative.css', { allChunks: true });
 
 module.exports = {
   entry: {
-    'app': './javascript/main.js',
-    'editor': './javascript/editor.js',
-    'theme_default': './sass/theme_default.scss',
-    'theme_alternative': './sass/theme_alternative.scss'
+    app: './javascript/main.js',
+    editor: './javascript/editor.js',
+    theme_default: './sass/theme_default.scss',
+    theme_alternative: './sass/theme_alternative.scss',
   },
   output: {
     path: __dirname + '/gen',
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
     loaders: [
       {
         test: /theme_default\.scss$/,
-        loader: extractThemeDefaultCSS.extract('style-loader', 'css-loader!sass-loader')
+        loader: extractDefault.extract('style-loader', 'css-loader!sass-loader'),
       },
       {
         test: /theme_alternative\.scss$/,
-        loader: extractThemeAlternativeCSS.extract('style-loader', 'css-loader!sass-loader')
+        loader: extractAlternative.extract('style-loader', 'css-loader!sass-loader'),
       },
       {
         test: [/ace-builds.*/, /.*ace-mode-beancount.*/],
-        loader: 'script-loader'
+        loader: 'script-loader',
       },
       {
-        test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader : 'file-loader'
-      }
-    ]
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel?presets[]=es2015',
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
     }),
-    extractThemeDefaultCSS,
-    extractThemeAlternativeCSS
+    extractDefault,
+    extractAlternative,
   ],
-}
+};
