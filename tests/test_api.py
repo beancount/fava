4b875ccb6d235789671b5bcd46811aae5e6ddf3d
Yuchen Ying <cnyegle@gmail.com>
2016-09-04 01:11:56 -0700

    Fix an issue that zero balance may not show on chart. (#356)
    When one of the commodities an account holds goes to zero, it will not
    show in `balance` field in the corresponding entry. A previous attempt
    to workaround the issue didn't consider when an account holds more than
    two commodities. This change fixed the workaround. Test case has been
    updated to cover it.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,10 @@
 def test_accounts(example_api):
-    assert len(example_api.all_accounts) == 91
-    assert len(example_api.all_accounts_active) == 60
+    assert len(example_api.all_accounts) == 93
+    assert len(example_api.all_accounts_active) == 61
     assert 'Assets' not in example_api.all_accounts_active
 
 
 def test_linechart_data(example_api):
-    data = example_api.linechart_data('Liabilities:AccountsPayable')
-    assert len(data) == 6
-    assert data[-1]['balance']['USD'] == 0
+    data = example_api.linechart_data('Assets:Testing:MultipleCommodities')
+    assert data[1]['balance'].get('USD', None) == 50
+    assert data[2]['balance'].get('USD', None) == 0
