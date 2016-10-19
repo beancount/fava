186b2badd8cc6f5023d2a146a152ddc0cd9eea71
Dominik Aumayr <dominik@aumayr.name>
2016-10-19 10:34:19 +0200

    Update script for pythonanywhere

--- contrib/pythonanywhere/fava/fava_pythonanywhere_com_wsgi.py
+++ contrib/pythonanywhere/fava/fava_pythonanywhere_com_wsgi.py
@@ -1,18 +1,5 @@
 from fava.application import app as application
-from fava.application import load_file, load_settings
+from fava.application import load_file
 
-application.config['BEANCOUNT_FILES'] = [
-    '/home/fava/test1.bean',
-    '/home/fava/test2.bean',
-    '/home/fava/test3.bean',
-]
-application.config['USER_SETTINGS'] = None
-
-load_settings()
+application.config['BEANCOUNT_FILES'] = ['/home/fava/test1.bean', '/home/fava/test2.bean', '/home/fava/test3.bean']
 load_file()
-
-# app.user_config = configparser.ConfigParser()
-# user_config_defaults_file = '/home/fava/default-settings.conf'
-# app.user_config.readfp(open(user_config_defaults_file))
-# app.user_config['fava']['file_defaults'] = user_config_defaults_file
-# app.user_config['fava']['file_user'] = ''
