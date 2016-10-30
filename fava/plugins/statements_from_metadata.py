29ad47e9dc9b794099734096fb90b16020467e41
Dominik Aumayr <dominik@aumayr.name>
2016-10-30 14:22:56 +0100

    Lint

--- fava/plugins/statements_from_metadata.py
+++ fava/plugins/statements_from_metadata.py
@@ -10,62 +10,69 @@ import collections
 from os.path import join, dirname, isfile
 from beancount.core import data
 
-StatementDocumentError = collections.namedtuple('StatementDocumentError', 'source message entry')
+StatementDocumentError = collections.namedtuple('StatementDocumentError',
+                                                'source message entry')
 
 __plugins__ = ['statements_from_metadata']
+
 
 def statements_from_metadata(entries, options_map):
     errors = []
     key = 'statement'
 
-    if not 'documents' in options_map or len(options_map['documents']) == 0:
+    if 'documents' not in options_map or len(options_map['documents']) == 0:
         return entries, errors
 
     def filenames(date, name):
-        return [name, '{} {}'.format(date, name), '{}.{}'.format(date, name)]
+        d_str = date.strftime("%Y-%m-%d")
+        return [name, '{} {}'.format(d_str, name), '{}.{}'.format(d_str, name)]
 
     for entry in entries:
         type = entry.__class__.__name__.lower()
-        if type == 'transaction':
-            possible_paths = []  # set of tuples (account, path)
-            date = entry.date.strftime("%Y-%m-%d")
 
-            for documents_root in options_map['documents']:
-                # If the `statement` metadata key is set on the transaction, assume it belongs to the first posting
-                if key in entry.meta:
-                    posting = entry.postings[0]
-                    account = posting.account.replace(':', '/')
-                    for filename in filenames(date, entry.meta[key]):
-                        possible_paths.append((posting.account, join(dirname(options_map['filename']), filename)))
-                        possible_paths.append((posting.account, join(dirname(options_map['filename']), documents_root, filename)))
-                        possible_paths.append((posting.account, join(dirname(options_map['filename']), documents_root, account, filename)))
+        if type != 'transaction':
+            continue
 
-                # Check if the `statement` metadata key is set on one of the postings
-                for posting in entry.postings:
-                    if posting.meta and key in posting.meta:
-                        account = posting.account.replace(':', '/')
-                        for filename in filenames(date, posting.meta[key]):
-                            possible_paths.append((posting.account, join(dirname(options_map['filename']), filename)))
-                            possible_paths.append((posting.account, join(dirname(options_map['filename']), documents_root, filename)))
-                            possible_paths.append((posting.account, join(dirname(options_map['filename']), documents_root, account, filename)))
+        paths = []  # set of tuples (account, path)
+        b_dir = dirname(options_map['filename'])
 
-            if len(possible_paths) == 0:
-                continue
+        for documents in options_map['documents']:
+            statements = []
 
-            found = False
-            for account, path in possible_paths:
-                if isfile(path):
-                    # TODO link the Document entry with the Transaction
-                    meta = data.new_metadata('<fava_statements_plugin>', 0)
-                    entries.append(data.Document(meta, entry.date, account, path))
-                    found = True
+            # If the `statement` metadata key is set on the transaction, assume
+            # it belongs to the first posting
+            if key in entry.meta:
+                statements.append((entry.meta[key], entry.postings[0].account))
 
-            if not found:
-                errors.append(
-                    StatementDocumentError(
-                        entry.meta,
-                        "Statement document not found. Search paths: {}".format(
-                            ', '.join([path for _, path in possible_paths])),
-                        entry))
+            for posting in entry.postings:
+                if posting.meta and key in posting.meta:
+                    statements.append((posting.meta[key], posting.account))
+
+            for path, account in statements:
+                account_path = account.replace(':', '/')
+                for filename in filenames(entry.date, path):
+                    paths.append((account, join(b_dir, filename)))
+                    paths.append((account, join(b_dir, documents, filename)))
+                    paths.append((account, join(b_dir, documents,
+                                                account_path, filename)))
+
+        if len(paths) == 0:
+            continue
+
+        found = False
+        for account, path in paths:
+            if isfile(path):
+                # TODO link the Document entry with the Transaction
+                meta = data.new_metadata('<fava_statements_plugin>', 0)
+                entries.append(data.Document(meta, entry.date, account, path))
+                found = True
+
+        if not found:
+            errors.append(
+                StatementDocumentError(
+                    entry.meta,
+                    "Document not found. Search paths: {}".format(
+                        ', '.join([path for _, path in paths])),
+                    entry))
 
     return entries, errors
