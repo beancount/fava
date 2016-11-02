1886fea5a66328063044eb452736f9f092e764e7
Dominik Aumayr <dominik@aumayr.name>
2016-11-02 18:58:20 +0100

    Support all document directives, not just autogenerated ones

--- fava/plugins/link_statements.py
+++ fava/plugins/link_statements.py
@@ -37,8 +37,7 @@ def link_statements(entries, options_map):
             statement_p = normpath(join(dirname(entry.meta['filename']),
                                         statement))
             documents = [(j, document) for j, document in all_documents
-                         if (document.filename == statement_p and
-                             document.meta['lineno'] == 0) or
+                         if (document.filename == statement_p) or
                             (document.account in
                                 [pos.account for pos in entry.postings] and
                              basename(document.filename) == statement)]
