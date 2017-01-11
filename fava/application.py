931536d8acc7c62721b5fbd21505db4054009413
Dominik Aumayr <dominik@aumayr.name>
2017-01-11 12:56:04 +0100

    Add Spanish to options and help pages

--- fava/application.py
+++ fava/application.py
@@ -93,7 +93,7 @@ def get_locale():
     """
     if g.ledger.fava_options['language']:
         return g.ledger.fava_options['language']
-    return request.accept_languages.best_match(['de', 'en'])
+    return request.accept_languages.best_match(['de', 'en', 'es'])
 
 
 for _, function in inspect.getmembers(template_filters, inspect.isfunction):
