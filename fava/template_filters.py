a635ca77cdc7f322fa90318f983d93e7b4f47e04
Jakob Schnitzer <mail@jakobschnitzer.de>
2017-10-09 21:05:22 +0200

    compute market value slightly differently

--- fava/template_filters.py
+++ fava/template_filters.py
@@ -6,9 +6,41 @@ All functions in this module will be automatically added as template filters.
 import os
 
 from flask import g
-from beancount.core import convert, compare, realization
+from beancount.core import compare
+from beancount.core import convert
 from beancount.core import data
+from beancount.core import prices
+from beancount.core import realization
+from beancount.core.amount import Amount
 from beancount.core.number import Decimal
+
+
+def get_market_value(pos, price_map, date=None):
+    """Get the market value of a Position.
+
+    This differs from the convert.get_value function in Beancount by returning
+    the cost value if no price can be found.
+
+    Args:
+        pos: A Position.
+        price_map: A dict of prices, as built by prices.build_price_map().
+        date: A datetime.date instance to evaluate the value at, or None.
+
+    Returns:
+        An Amount, with value converted or if the conversion failed just the
+        cost value (or the units if the position has no cost).
+    """
+    units_ = pos.units
+    cost_ = pos.cost
+    value_currency = cost_.currency if cost_ else None
+
+    if value_currency:
+        base_quote = (units_.currency, value_currency)
+        _, price_number = prices.get_price(price_map, base_quote, date)
+        if price_number is not None:
+            return Amount(units_.number * price_number, value_currency)
+        return Amount(units_.number * cost_.number, value_currency)
+    return units_
 
 
 def remove_keys(_dict, keys):
@@ -34,7 +66,7 @@ def cost(inventory):
 def cost_or_value(inventory, date=None):
     """Get the cost or value of an inventory."""
     if g.conversion == 'at_value':
-        return inventory.reduce(convert.get_value, g.ledger.price_map, date)
+        return inventory.reduce(get_market_value, g.ledger.price_map, date)
     if g.conversion:
         return inventory.reduce(convert.convert_position, g.conversion,
                                 g.ledger.price_map, date)
