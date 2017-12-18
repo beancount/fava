7fb4a964b21dac38ec64b21ed30f8aefd44d3bf6
Bala Ramadurai <bala@balaramadurai.net>
2017-12-18 22:54:08 +0530

    rendering international commas
    default value of     'locale': 'en_US.UTF8' for "normal" grouping. This value can be changed in the beancount file with "fava-options" "locale"

--- fava/core/fava_options.py
+++ fava/core/fava_options.py
@@ -29,6 +29,7 @@ DEFAULTS = {
     'journal-show-document': ['discovered', 'statement'],
     'journal-show-transaction': ['cleared', 'pending'],
     'language': None,
+    'locale': 'en_US.UTF8',
     'show-accounts-with-zero-balance': True,
     'show-accounts-with-zero-transactions': True,
     'show-closed-accounts': False,
@@ -65,6 +66,7 @@ LIST_OPTS = [
 ]
 
 STR_OPTS = [
+    'locale',
     'import-config',
     'interval',
     'language',
