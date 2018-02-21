6f65dafc4f5a8d8541488fe1b61e9c609fde6a38
Jakob Schnitzer <mail@jakobschnitzer.de>
2018-02-21 11:57:42 -0300

    json_api: minor refactorings

--- fava/json_api.py
+++ fava/json_api.py
@@ -1,6 +1,7 @@
 """JSON API."""
 
 import os
+import functools
 
 from flask import Blueprint, jsonify, g, request
 from werkzeug.utils import secure_filename
@@ -17,68 +18,94 @@ from fava.core.misc import extract_tags_links
 json_api = Blueprint('json_api', __name__)  # pylint: disable=invalid-name
 
 
-def _api_success(**kwargs):
-    kwargs['success'] = True
-    return jsonify(kwargs)
+def json_response(func):
+    """Jsonify the response."""
+
+    @functools.wraps(func)
+    def _wrapper(*args, **kwargs):
+        json_data = func(*args, **kwargs)
+        if 'success' not in json_data:
+            json_data['success'] = True
+        return jsonify(json_data)
+
+    return _wrapper
+
+
+def json_request(func):
+    """Check existence and load the JSON payload of the request."""
+
+    @functools.wraps(func)
+    def _wrapper():
+        request_data = request.get_json()
+        if request_data is None:
+            raise FavaAPIException('Invalid JSON request.')
+        return func(request_data)
+
+    return _wrapper
 
 
 @json_api.errorhandler(FavaAPIException)
+@json_response
 def _json_api_exception(error):
-    return jsonify({'success': False, 'error': error.message})
+    return {'success': False, 'error': error.message}
 
 
 @json_api.errorhandler(OSError)
+@json_response
 def _json_api_oserror(error):
-    return jsonify({'success': False, 'error': error.strerror})
+    return {'success': False, 'error': error.strerror}
 
 
 @json_api.route('/changed/')
+@json_response
 def changed():
     """Check for file changes."""
-    return jsonify({'success': True, 'changed': g.ledger.changed()})
+    return {'changed': g.ledger.changed()}
 
 
 @json_api.route('/errors/')
+@json_response
 def errors():
     """Number of errors."""
-    return jsonify({'success': True, 'errors': len(g.ledger.errors)})
+    return {'errors': len(g.ledger.errors)}
 
 
 @json_api.route('/source/', methods=['PUT'])
-def source():
-    """Read/write one of the source files."""
-    request_data = request.get_json()
-    if request_data is None:
-        raise FavaAPIException('Invalid JSON request.')
+@json_request
+@json_response
+def source(request_data):
+    """Write one of the source files."""
     if request_data.get('file_path'):
         sha256sum = g.ledger.file.set_source(
-            request_data.get('file_path'),
-            request_data.get('source'), request_data.get('sha256sum'))
+            request_data.get('file_path'), request_data.get('source'),
+            request_data.get('sha256sum'))
     else:
         entry = g.ledger.get_entry(request_data.get('entry_hash'))
-        sha256sum = save_entry_slice(entry,
-                                     request_data.get('source'),
+        sha256sum = save_entry_slice(entry, request_data.get('source'),
                                      request_data.get('sha256sum'))
-    return _api_success(sha256sum=sha256sum)
+    return {'sha256sum': sha256sum}
 
 
 @json_api.route('/format-source/', methods=['POST'])
-def format_source():
+@json_request
+@json_response
+def format_source(request_data):
     """Format beancount file."""
-    request_data = request.get_json()
-    if request_data is None:
-        raise FavaAPIException('Invalid JSON request.')
-    return _api_success(payload=align_beancount(request_data['source']))
+    return {'payload': align_beancount(request_data['source'])}
 
 
 @json_api.route('/payee-accounts/', methods=['GET'])
+@json_response
 def payee_accounts():
     """Rank accounts for the given payee."""
-    return _api_success(
-        payload=g.ledger.attributes.payee_accounts(request.args.get('payee')))
+    return {
+        'payload': g.ledger.attributes.payee_accounts(
+            request.args.get('payee'))
+    }
 
 
 @json_api.route('/add-document/', methods=['PUT'])
+@json_response
 def add_document():
     """Upload a document."""
     if not g.ledger.options['documents']:
@@ -104,8 +131,7 @@ def add_document():
     directory = os.path.normpath(
         os.path.join(
             os.path.dirname(g.ledger.beancount_file_path), documents_folder,
-            *request.form['account'].split(':')
-        ))
+            *request.form['account'].split(':')))
     filepath = os.path.join(directory, filename)
 
     if os.path.exists(filepath):
@@ -119,7 +145,7 @@ def add_document():
     if request.form.get('hash'):
         g.ledger.file.insert_metadata(request.form['hash'], 'statement',
                                       filename)
-    return _api_success(message='Uploaded to {}'.format(filepath))
+    return {'message': 'Uploaded to {}'.format(filepath)}
 
 
 def _parse_number(num):
@@ -146,8 +172,8 @@ def json_to_entry(json_entry, valid_accounts):
 
         for posting in json_entry['postings']:
             if posting['account'] not in valid_accounts:
-                raise FavaAPIException('Unknown account: {}.'
-                                       .format(posting['account']))
+                raise FavaAPIException('Unknown account: {}.'.format(
+                    posting['account']))
             data.create_simple_posting(txn, posting['account'],
                                        _parse_number(posting.get('number')),
                                        posting.get('currency'))
@@ -155,8 +181,8 @@ def json_to_entry(json_entry, valid_accounts):
         return txn
     elif json_entry['type'] == 'balance':
         if json_entry['account'] not in valid_accounts:
-            raise FavaAPIException(
-                'Unknown account: {}.'.format(json_entry['account']))
+            raise FavaAPIException('Unknown account: {}.'.format(
+                json_entry['account']))
         number = _parse_number(json_entry['number'])
         amount = Amount(number, json_entry.get('currency'))
 
@@ -164,8 +190,8 @@ def json_to_entry(json_entry, valid_accounts):
                             json_entry['account'], amount, None, None)
     elif json_entry['type'] == 'note':
         if json_entry['account'] not in valid_accounts:
-            raise FavaAPIException(
-                'Unknown account: {}.'.format(json_entry['account']))
+            raise FavaAPIException('Unknown account: {}.'.format(
+                json_entry['account']))
 
         if '"' in json_entry['comment']:
             raise FavaAPIException('Note contains double-quotes (")')
@@ -177,12 +203,10 @@ def json_to_entry(json_entry, valid_accounts):
 
 
 @json_api.route('/add-entries/', methods=['PUT'])
-def add_entries():
+@json_request
+@json_response
+def add_entries(request_data):
     """Add multiple entries."""
-    request_data = request.get_json()
-    if request_data is None:
-        raise FavaAPIException('Invalid JSON request.')
-
     try:
         entries = [
             json_to_entry(entry, g.ledger.attributes.accounts)
@@ -193,4 +217,4 @@ def add_entries():
 
     g.ledger.file.insert_entries(entries)
 
-    return _api_success(message='Stored {} entries.'.format(len(entries)))
+    return {'message': 'Stored {} entries.'.format(len(entries))}
