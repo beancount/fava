a8a6154874af4f429a15723b289b76074d7f577e
Jakob Schnitzer <mail@jakobschnitzer.de>
2018-03-13 15:46:20 +0100

    use rollup to copy fonts

--- fava/static/rollup.config.js
+++ fava/static/rollup.config.js
@@ -1,10 +1,10 @@
 import buble from 'rollup-plugin-buble';
 import commonjs from 'rollup-plugin-commonjs';
+import copy from 'rollup-plugin-copy';
 import json from 'rollup-plugin-json';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 
-import postcssCopy from 'postcss-copy';
 import postcssImport from 'postcss-import';
 import postcssNesting from 'postcss-nesting';
 
@@ -21,13 +21,19 @@ export default {
     nodeResolve(),
     json(),
     buble({ exclude: 'css/**' }),
+    copy({
+      'node_modules/@typopro/web-fira-mono/TypoPRO-FiraMono-Medium.woff': 'gen/FiraMono-Medium.woff',
+      'node_modules/@typopro/web-fira-mono/TypoPRO-FiraMono-Regular.woff': 'gen/FiraMono-Regular.woff',
+      'node_modules/@typopro/web-fira-sans/TypoPRO-FiraSans-Medium.woff': 'gen/FiraSans-Medium.woff',
+      'node_modules/@typopro/web-fira-sans/TypoPRO-FiraSans-Regular.woff': 'gen/FiraSans-Regular.woff',
+      'node_modules/@typopro/web-source-code-pro/TypoPRO-SourceCodePro-Regular.woff': 'gen/SourceCodePro-Regular.woff',
+      'node_modules/@typopro/web-source-code-pro/TypoPRO-SourceCodePro-Semibold.woff': 'gen/SourceCodePro-Semibold.woff',
+      'node_modules/@typopro/web-source-serif-pro/TypoPRO-SourceSerifPro-Regular.woff': 'gen/SourceSerifPro-Regular.woff',
+      'node_modules/@typopro/web-source-serif-pro/TypoPRO-SourceSerifPro-Semibold.woff': 'gen/SourceSerifPro-Semibold.woff',
+    }),
     postcss({
       plugins: [
         postcssImport(),
-        postcssCopy({
-          src: ['css', 'node_modules'],
-          dest: 'gen',
-        }),
         postcssNesting(),
       ],
       extract: true,
