aac8bcf0397b2e9112f0c45d7c4b73b3cbabd2de
Jakob Schnitzer <mail@jakobschnitzer.de>
2018-04-01 11:52:25 +0200

    js: no need for library to copy to clipboard

--- fava/static/javascript/clipboard.js
+++ fava/static/javascript/clipboard.js
@@ -1,8 +1,36 @@
-import Clipboard from 'clipboard';
-
 import e from './events';
+import { $, $$ } from './helpers';
 
-e.on('page-init', () => {
-  new Clipboard('.status-indicator'); // eslint-disable-line no-new
-  new Clipboard('#copy-balances'); // eslint-disable-line no-new
+// Copy the given text to the clipboard.
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  textarea.style.position = 'fixed';
+  textarea.style.top = '0';
+  textarea.style.left = '0';
+  document.body.appendChild(textarea);
+  textarea.focus();
+  textarea.select();
+
+  try {
+    document.execCommand('copy');
+  } catch (err) {
+    console.error('Unable to copy', err); // eslint-disable-line no-console
+  }
+  textarea.remove();
+}
+
+e.on('page-loaded', () => {
+  $$('.status-indicator').forEach((indicator) => {
+    indicator.addEventListener('click', () => {
+      copyToClipboard(indicator.getAttribute('data-clipboard-text'));
+    });
+  });
+
+  const copyBalances = $('#copy-balances');
+  if (copyBalances) {
+    copyBalances.addEventListener('click', () => {
+      copyToClipboard(copyBalances.getAttribute('data-clipboard-text'));
+    });
+  }
 });
