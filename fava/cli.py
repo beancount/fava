665e840c957e3ab13c302e9774b081a2e77464e2
Lin <lin@anticlack.com>
2018-08-02 13:57:29 -0500

    Fix documentation line length for lint

--- fava/cli.py
+++ fava/cli.py
@@ -35,8 +35,8 @@ def main(filenames, port, host, prefix, incognito, debug, profile,
          profile_dir):
     """Start Fava for FILENAMES on http://<host>:<port>.
 
-    If the `BEANCOUNT_FILE` environment variable is set, Fava will use the files (space-delimited_
-    specified there in addition to FILENAMES.
+    If the `BEANCOUNT_FILE` environment variable is set, Fava will use the
+    files (space-delimited) specified there in addition to FILENAMES.
     """
 
     if profile:  # pragma: no cover
