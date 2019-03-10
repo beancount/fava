7f18c951f5eb30dfb657466f94f5495265cecf5e
Balázs Keresztury <b@belidzs.hu>
2019-03-10 19:19:59 +0100

    Fixed linting errors

--- tests/test_core_file.py
+++ tests/test_core_file.py
@@ -319,12 +319,12 @@ def test_utf8_content(utf8_ledger, tmpdir):
     * insert_enty()
     """
     entry = utf8_ledger.get_entry("b11cd783fba370a9da24f1e54e855e06")
-    entry_slice, sha256sum = get_entry_slice(entry)
+    sha256sum = get_entry_slice(entry)[1]
     entry_source = """2019-03-10 * "Árvíztűrő tükörfúrógép" "Árvíztűrő tükörfúrógép"
     Expenses:Example  100 HUF
     Assets:Example -100 HUF"""
     save_entry_slice(entry, entry_source, sha256sum)
-    f = tmpdir.mkdir("test_utf8_content").join("utf8.beancount")
-    f.write_text(entry_source, encoding="utf-8")
-    insert_metadata_in_file(f, 0, 'test', 'value')
-    insert_entry(entry, [f], fava_options={})
+    tmpfile = tmpdir.mkdir("test_utf8_content").join("utf8.beancount")
+    tmpfile.write_text(entry_source, encoding="utf-8")
+    insert_metadata_in_file(tmpfile, 0, "test", "value")
+    insert_entry(entry, [tmpfile], fava_options={})
