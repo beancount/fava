cf96d60de0b1dc76f8428a4cb34cdea6f75a8934
Jakob Schnitzer <mail@jakobschnitzer.de>
2020-10-06 18:19:57 +0200

    eslint: misc

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,8 +1,25 @@
+const airbnbConfigOverrides = {
+  "max-classes-per-file": 0,
+  "no-param-reassign": ["error", { props: false }],
+  "no-restricted-syntax": [
+    "error",
+    "ForInStatement",
+    "LabelStatement",
+    "WithStatement",
+  ],
+  "no-underscore-dangle": 0,
+  "import/extensions": 0,
+  "import/no-unresolved": 0,
+  "import/prefer-default-export": 0,
+  "@typescript-eslint/naming-convention": 0,
+};
+
 module.exports = {
   plugins: ["@typescript-eslint", "svelte3"],
   extends: [
     "airbnb-typescript/base",
     "plugin:@typescript-eslint/recommended",
+    // "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
     "prettier/@typescript-eslint",
   ],
@@ -14,22 +31,8 @@ module.exports = {
     browser: true,
   },
   rules: {
-    camelcase: 0,
+    ...airbnbConfigOverrides,
     curly: ["error", "all"],
-    "max-classes-per-file": 0,
-    "no-param-reassign": 0,
-    "no-restricted-syntax": [
-      "error",
-      "ForInStatement",
-      "LabelStatement",
-      "WithStatement",
-    ],
-    "no-underscore-dangle": 0,
-    "import/extensions": 0,
-    "import/no-unresolved": 0,
-    "import/prefer-default-export": 0,
-    "@typescript-eslint/camelcase": 0,
-    "@typescript-eslint/naming-convention": 0,
   },
   overrides: [
     {
