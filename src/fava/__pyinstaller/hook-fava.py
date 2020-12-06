0b697ff8a8c2c23d7a60a0393e6fc9848fd434a7
johannesjh <johannesjh@users.noreply.github.com>
2020-12-06 19:27:27 +0100

    Fixes pylint / mypy error

--- src/fava/__pyinstaller/hook-fava.py
+++ src/fava/__pyinstaller/hook-fava.py
@@ -1,12 +1,10 @@
+# pylint: skip-file
+# mypy: ignore-errors
 """
 This is a PyInstaller hook needed to successfully freeze fava.
 """
-from PyInstaller.utils.hooks import (
-    collect_data_files,
-)  # pylint: disable=import-error
-from PyInstaller.utils.hooks import (
-    copy_metadata,
-)  # pylint: disable=import-error
+from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import copy_metadata
 
 datas = collect_data_files("fava")
 datas += copy_metadata("fava")
