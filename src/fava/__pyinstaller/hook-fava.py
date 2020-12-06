2b8e8fafccf77270877807495ab01bbd6fdb00b3
johannesjh <johannesjh@users.noreply.github.com>
2020-12-06 19:18:01 +0100

    Fixes pylint error about missing pyinstaller imports

--- src/fava/__pyinstaller/hook-fava.py
+++ src/fava/__pyinstaller/hook-fava.py
@@ -1,8 +1,12 @@
 """
 This is a PyInstaller hook needed to successfully freeze fava.
 """
-from PyInstaller.utils.hooks import collect_data_files
-from PyInstaller.utils.hooks import copy_metadata
+from PyInstaller.utils.hooks import (
+    collect_data_files,
+)  # pylint: disable=import-error
+from PyInstaller.utils.hooks import (
+    copy_metadata,
+)  # pylint: disable=import-error
 
 datas = collect_data_files("fava")
 datas += copy_metadata("fava")
