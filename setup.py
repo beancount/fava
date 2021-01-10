5e83b365eceba27c11daac638637d85bcca96b87
johannesjh <johannesjh@users.noreply.github.com>
2021-01-10 16:13:52 +0100

    Adds custom setup.py build command that builds fava's frontend.

--- setup.py
+++ setup.py
@@ -2,6 +2,40 @@
 
 The configuration is in setup.cfg.
 """
-from setuptools import setup
+import subprocess
+from distutils.cmd import Command
+from distutils.log import INFO
 
-setup()
+from setuptools import setup
+from setuptools.command.build_py import build_py
+
+
+class BuildFrontend(Command):
+    """Custom command `setup.py build_frontend` to build fava's frontend."""
+
+    def initialize_options(self) -> None:
+        pass
+
+    def finalize_options(self) -> None:
+        pass
+
+    def run(self) -> None:
+        command = ["make"]
+        self.announce(f"Running command {command}", level=INFO)
+        subprocess.check_call(command)
+
+
+class BuildPy(build_py):
+    """Custom command `setup.py build` to build fava including its frontend."""
+
+    def run(self):
+        self.run_command("build_frontend")
+        super().run()
+
+
+setup(
+    cmdclass={
+        "build_frontend": BuildFrontend,
+        "build_py": BuildPy,
+    }
+)
