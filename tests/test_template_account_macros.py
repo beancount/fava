36b723f4c74ae1596be62266ed2840ed7e0ab9b0
Jakob Schnitzer <mail@jakobschnitzer.de>
2021-01-24 09:25:33 +0100

    account_macros: more tests

--- tests/test_template_account_macros.py
+++ tests/test_template_account_macros.py
@@ -11,3 +11,31 @@ def test_indicator(app, example_ledger, snapshot) -> None:
         yellow_status = macro(example_ledger, "Assets:US:BofA:Checking")
         assert "yellow" in yellow_status
         snapshot(yellow_status)
+
+
+def test_balance_directive(app, example_ledger) -> None:
+    bal = "2021-01-24 balance Assets:US:BofA:Checking              1632.79 USD"
+    with app.test_request_context(""):
+        macro = get_template_attribute(
+            "macros/_account_macros.html", "balance_directive"
+        )
+        assert macro(example_ledger, "NONEXISTING") == ""
+        assert macro(example_ledger, "Assets:US:BofA:Checking") == f"{bal}\n"
+
+
+def test_account_name(app, example_ledger) -> None:
+    with app.test_request_context("/long-example/"):
+        app.preprocess_request()
+        macro = get_template_attribute(
+            "macros/_account_macros.html", "account_name"
+        )
+        assert (
+            macro(example_ledger, "NONEXISTING")
+            == '<a href="/long-example/account/NONEXISTING/"'
+            ' class="account">NONEXISTING</a>\n'
+        )
+        assert (
+            macro(example_ledger, "Assets:US:BofA:Checking")
+            == '<a href="/long-example/account/Assets:US:BofA:Checking/"'
+            ' class="account">Assets:US:BofA:Checking</a>\n'
+        )
