1dce232fa9045da09aa71a7d786c90bf503fcd49
Jakob Schnitzer <mail@jakobschnitzer.de>
2021-01-25 17:48:16 +0100

    fix date-dependent test

--- tests/test_template_account_macros.py
+++ tests/test_template_account_macros.py
@@ -1,8 +1,10 @@
 # pylint: disable=missing-docstring
+import datetime
+
 from flask import get_template_attribute
 
 
-def test_indicator(app, example_ledger, snapshot) -> None:
+def test_indicator(app, example_ledger) -> None:
     with app.test_request_context(""):
         macro = get_template_attribute(
             "macros/_account_macros.html", "indicator"
@@ -10,17 +12,17 @@ def test_indicator(app, example_ledger, snapshot) -> None:
         assert macro(example_ledger, "NONEXISTING") == ""
         yellow_status = macro(example_ledger, "Assets:US:BofA:Checking")
         assert "yellow" in yellow_status
-        snapshot(yellow_status)
 
 
 def test_balance_directive(app, example_ledger) -> None:
-    bal = "2021-01-24 balance Assets:US:BofA:Checking              1632.79 USD"
+    today = datetime.date.today()
+    bal = f"{today} balance Assets:US:BofA:Checking              1632.79 USD\n"
     with app.test_request_context(""):
         macro = get_template_attribute(
             "macros/_account_macros.html", "balance_directive"
         )
         assert macro(example_ledger, "NONEXISTING") == ""
-        assert macro(example_ledger, "Assets:US:BofA:Checking") == f"{bal}\n"
+        assert macro(example_ledger, "Assets:US:BofA:Checking") == f"{bal}"
 
 
 def test_account_name(app, example_ledger) -> None:
