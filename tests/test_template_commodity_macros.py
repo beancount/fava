753855c0293cec85362029bda9dbef11e33c040c
Jakob Schnitzer <mail@jakobschnitzer.de>
2021-02-12 18:23:30 +0100

    render_amount: fix closing tag in macro, improve tests

--- tests/test_template_commodity_macros.py
+++ tests/test_template_commodity_macros.py
@@ -12,11 +12,17 @@ def test_render_currency(app, example_ledger) -> None:
             COMMODITY_MACROS_PATH, "render_currency"
         )
         assert "US Dollar" in macro(example_ledger, "USD")
+        test = '<span title="TEST">TEST</span>'
+        assert macro(example_ledger, "TEST") == test
 
 
 def test_render_amount(app, example_ledger) -> None:
     with app.test_request_context("/long-example/"):
         app.preprocess_request()
         macro = get_template_attribute(COMMODITY_MACROS_PATH, "render_amount")
-        amount = A("10 USD")
-        assert "US Dollar" in macro(example_ledger, amount)
+        res = '<span class="num" title="US Dollar">10.00 USD</span>'
+        assert macro(example_ledger, A("10 USD")) == res
+        res = '<span class="num"></span>'
+        assert macro(example_ledger, None) == res
+        res = '<span class="num" title="TEST">10.00 TEST</span>'
+        assert macro(example_ledger, A("10 TEST")) == res
