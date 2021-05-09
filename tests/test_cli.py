546c225f1d2950d139662f03271e8de54b6d6e85
Jakob Schnitzer <mail@jakobschnitzer.de>
2021-05-09 12:49:18 +0200

    appease pylint
    Use the context manager form of subprocess.Popen in tests.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,12 @@
 # pylint: disable=missing-docstring
 import os
-import socket
-import subprocess
 import sys
-import time
+from socket import socket
+from subprocess import PIPE
+from subprocess import Popen
+from subprocess import STDOUT
+from time import sleep
+from time import time
 
 import pytest
 
@@ -16,42 +19,43 @@ if "BEANCOUNT_FILE" in os.environ:
     del os.environ["BEANCOUNT_FILE"]
 
 
-def get_port():
-    sock = socket.socket()
+def get_port() -> int:
+    sock = socket()
     sock.bind((HOST, 0))
     port = sock.getsockname()[1]
     sock.close()
     return port
 
 
-def output_contains(process, output, timeout):
-    endtime = time.time() + timeout
+def output_contains(process: Popen, output: str, timeout: int) -> bool:
+    endtime = time() + timeout
     while True:
-        if time.time() > endtime:
+        if time() > endtime or not process.stdout:
             return False
         if output in process.stdout.readline():
             return True
-        time.sleep(0.1)
-
-
-def run_fava(args=()):
-    return subprocess.Popen(
-        FAVA + args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        universal_newlines=True,
-    )
+        sleep(0.1)
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="does not run on windows")
-def test_cli():
+def test_cli() -> None:
     port = str(get_port())
     args = (EXAMPLE_FILE, "-d", "-p", port)
-    process = run_fava(args)
-    assert output_contains(process, "Running on", 20)
-    process2 = run_fava(args)
-    process2.wait()
-    process.terminate()
-    process.wait()
-    assert "in use" in "".join(process2.stdout.readlines())
-    assert process2.returncode == 2
+    with Popen(
+        FAVA + args,
+        stdout=PIPE,
+        stderr=STDOUT,
+        universal_newlines=True,
+    ) as process:
+        assert output_contains(process, "Running on", 20)
+        with Popen(
+            FAVA + args,
+            stdout=PIPE,
+            stderr=STDOUT,
+            universal_newlines=True,
+        ) as process2:
+            process2.wait()
+            process.terminate()
+            assert process2.stdout
+            assert "in use" in "".join(process2.stdout.readlines())
+            assert process2.returncode == 2
