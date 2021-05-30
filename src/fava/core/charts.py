c84b13d3d2e51a0b1515209c8d24679f322d4393
Jakob Schnitzer <mail@jakobschnitzer.de>
2021-05-30 09:46:57 +0200

    fixup lint

--- src/fava/core/charts.py
+++ src/fava/core/charts.py
@@ -158,6 +158,7 @@ class ChartModule(FavaModule):
                 )
 
             if invert:
+                # pylint: disable=invalid-unary-operand-type
                 balance = -balance
                 budgets = {k: -v for k, v in budgets.items()}
 
