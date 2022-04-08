7f3b5b2e0a1c23291adc02a22154d2de681f786b
PhracturedBlue <rc2012@pblue.org>
2022-04-08 14:35:03 -0700

    Initial stacked barchart implementation

--- frontend/src/charts/BarChart.svelte
+++ frontend/src/charts/BarChart.svelte
@@ -1,15 +1,25 @@
 <script lang="ts">
-  import { extent, merge } from "d3-array";
+  import { filter, max, merge, min, sum } from "d3-array";
   import { axisBottom, axisLeft } from "d3-axis";
-  import { scaleBand, scaleLinear } from "d3-scale";
+  import { scaleBand, scaleLinear, scaleOrdinal } from "d3-scale";
+  import { stack, stackOffsetDiverging } from "d3-shape";
+  import type { Series } from "d3-shape";
   import { getContext } from "svelte";
   import type { Writable } from "svelte/store";
 
   import { ctx } from "../format";
+  import { urlForAccount } from "../helpers";
+  import router from "../router";
+  import { barChartMode } from "../stores/chart";
 
   import Axis from "./Axis.svelte";
-  import type { BarChart, BarChartDatumValue } from "./bar";
-  import { currenciesScale, filterTicks, setTimeFilter } from "./helpers";
+  import type { BarChart, BarChartDatum, BarChartDatumValue } from "./bar";
+  import {
+    currenciesScale,
+    filterTicks,
+    hclColorRange,
+    setTimeFilter,
+  } from "./helpers";
   import { followingTooltip } from "./tooltip";
 
   export let data: BarChart["data"];
@@ -25,6 +35,41 @@
     left: 40,
   };
   const height = 250;
+  let hasStackedData = false;
+
+  function build_stack(_data: BarChartDatum[]) {
+    const accounts = new Map<string, Series<BarChartDatum, string>[]>(
+      allAccounts.map((d) => [d, []])
+    );
+    if (accounts.size > 1) {
+      hasStackedData = true;
+    }
+    for (let cur_idx = 0; cur_idx < _data[0].values.length; cur_idx += 1) {
+      const bar_stack = stack<BarChartDatum>()
+        .keys(accounts.keys())
+        .value((obj, key) => obj.values[cur_idx].children.get(key) ?? 0)
+        .offset(stackOffsetDiverging)(_data);
+      for (let acct_idx = 0; acct_idx < bar_stack.length; acct_idx += 1) {
+        accounts.get(bar_stack[acct_idx].key)?.push(bar_stack[acct_idx]);
+      }
+    }
+    return accounts;
+  }
+
+  $: allAccounts = Array.from(
+    new Set<string>(
+      data
+        .map<string[]>((d: BarChartDatum) =>
+          d.values
+            .map<string[]>((e: BarChartDatumValue) =>
+              Array.from(e.children.keys())
+            )
+            .flat()
+        )
+        .flat()
+    )
+  );
+  $: stacked_data = build_stack(data);
   $: innerHeight = height - margin.top - margin.bottom;
   $: maxWidth = data.length * maxColumnWidth;
   $: offset = margin.left + Math.max(0, width - maxWidth) / 2;
@@ -38,22 +83,41 @@
   $: x1 = scaleBand()
     .domain(data[0].values.map((d) => d.name))
     .range([0, x0.bandwidth()]);
-  let yMin: number;
-  let yMax: number;
-  $: [yMin = 0, yMax = 0] = extent(
-    merge<BarChartDatumValue>(data.map((d) => d.values)),
-    (d) => d.value
-  );
+  $: stackedBar = $barChartMode === "stacked" && hasStackedData;
+  $: yMin = stackedBar
+    ? min(
+        merge<number>(
+          data.map((b) =>
+            b.values.map((c) => sum(filter(c.children.values(), (d) => d < 0)))
+          )
+        )
+      )
+    : min(merge<BarChartDatumValue>(data.map((d) => d.values)), (d) => d.value);
+  $: yMax = stackedBar
+    ? max(
+        merge<number>(
+          data.map((b) =>
+            b.values.map((c) => sum(filter(c.children.values(), (d) => d > 0)))
+          )
+        )
+      )
+    : max(merge<BarChartDatumValue>(data.map((d) => d.values)), (d) => d.value);
   $: y = scaleLinear()
     .range([innerHeight, 0])
-    .domain([Math.min(0, yMin), Math.max(0, yMax)]);
+    .domain([Math.min(0, yMin ?? 0), Math.max(0, yMax ?? 0)]);
+
+  $: colorScale = scaleOrdinal<string, string>()
+    .domain(allAccounts)
+    .range(hclColorRange(allAccounts.length));
 
   const legend: Writable<[string, string][]> = getContext("chart-legend");
   $: legend.set(
-    x1
-      .domain()
-      .sort()
-      .map((c) => [c, $currenciesScale(c)])
+    stackedBar
+      ? []
+      : x1
+          .domain()
+          .sort()
+          .map((c) => [c, $currenciesScale(c)])
   );
 
   // Axes
@@ -61,6 +125,7 @@
     .tickSizeOuter(0)
     .tickValues(filterTicks(x0.domain(), innerWidth / 70));
   $: yAxis = axisLeft(y).tickSize(-innerWidth).tickFormat($ctx.short);
+  let highlighted = "";
 </script>
 
 <svg {width} {height}>
@@ -71,7 +136,7 @@
       <g
         class="group"
         class:desaturate={group.date > today}
-        use:followingTooltip={() => tooltipText($ctx, group)}
+        use:followingTooltip={() => tooltipText($ctx, group, "")}
         transform={`translate(${x0(group.label)},0)`}
       >
         <rect
@@ -89,28 +154,76 @@
           width={x0.bandwidth()}
           height={margin.bottom}
         />
-        {#each group.values as bar}
-          <rect
-            fill={$currenciesScale(bar.name)}
-            width={x1.bandwidth()}
-            x={x1(bar.name)}
-            y={y(Math.max(0, bar.value))}
-            height={Math.abs(y(bar.value) - y(0))}
-          />
-          <rect
-            class="budget"
-            width={x1.bandwidth()}
-            x={x1(bar.name)}
-            y={y(Math.max(0, bar.budget))}
-            height={Math.abs(y(bar.budget) - y(0))}
-          />
-        {/each}
+        {#if !stackedBar}
+          {#each group.values as bar}
+            <rect
+              fill={$currenciesScale(bar.name)}
+              width={x1.bandwidth()}
+              x={x1(bar.name)}
+              y={y(Math.max(0, bar.value))}
+              height={Math.abs(y(bar.value) - y(0))}
+            />
+            <rect
+              class="budget"
+              width={x1.bandwidth()}
+              x={x1(bar.name)}
+              y={y(Math.max(0, bar.budget))}
+              height={Math.abs(y(bar.budget) - y(0))}
+            />
+          {/each}
+        {/if}
       </g>
     {/each}
+    {#if stackedBar}
+      {#each [...stacked_data] as [name, account]}
+        <g
+          class="category"
+          class:faded={name !== highlighted && highlighted !== ""}
+        >
+          {#each account as group, currency_idx}
+            {#each group as bar}
+              {#if !Number.isNaN(bar[1])}
+                <rect
+                  class:desaturate={bar.data.date > today}
+                  width={x1.bandwidth()}
+                  x={(x0(bar.data.label) ?? 0) +
+                    (x1(bar.data.values[currency_idx].name) ?? 0)}
+                  y={y(Math.max(bar[0], bar[1]))}
+                  height={Math.abs(y(bar[1]) - y(bar[0]))}
+                  fill={colorScale(name)}
+                  on:mouseover={() => {
+                    highlighted = name;
+                  }}
+                  on:focus={() => {
+                    highlighted = name;
+                  }}
+                  on:mouseout={() => {
+                    highlighted = "";
+                  }}
+                  on:blur={() => {
+                    highlighted = "";
+                  }}
+                  use:followingTooltip={() => tooltipText($ctx, bar.data, name)}
+                  on:click={() => {
+                    if (!name.startsWith(":")) {
+                      router.navigate(urlForAccount(name));
+                    }
+                  }}
+                />
+              {/if}
+            {/each}
+          {/each}
+        </g>
+      {/each}
+    {/if}
   </g>
 </svg>
 
 <style>
+  .category.faded {
+    opacity: 0.5;
+  }
+
   .axis-group-box {
     cursor: pointer;
     opacity: 0;
--- src/fava/core/charts.py
+++ src/fava/core/charts.py
@@ -17,6 +17,7 @@ from beancount.core.data import Transaction
 from beancount.core.inventory import Inventory
 from beancount.core.number import Decimal
 from beancount.core.position import Position
+from flask_babel import gettext  # type: ignore
 from simplejson import JSONEncoder
 
 from fava.core._compat import FLAG_UNREALIZED
@@ -29,7 +30,6 @@ from fava.helpers import FavaAPIException
 from fava.util import listify
 from fava.util import pairwise
 from fava.util.date import Interval
-
 
 ONE_DAY = timedelta(days=1)
 
@@ -92,6 +92,7 @@ class DateAndBalanceWithBudget:
 
     date: date
     balance: Inventory
+    account_balances: dict[str, Inventory]
     budgets: dict[str, Decimal]
 
 
@@ -129,6 +130,38 @@ class ChartModule(FavaModule):
             if prices:
                 yield base, quote, prices
 
+    def _consolidate_accounts(
+        self, account_balances: dict[str, Any], count: int
+    ) -> None:
+        """Consolidate accounts into <= count buckets"""
+        # mypy doesn't understand that Inventory has __abs__ and add_inventory
+        # so declare as Any to bypass type checking
+        def sort_func(key: Any) -> Any:
+            return abs(account_balances[key])
+
+        consolidated_balances: dict[str, Any] = {}
+        ordered_accounts = sorted(
+            account_balances.keys(),
+            key=sort_func,
+            reverse=True,
+        )
+        while len(ordered_accounts) + len(consolidated_balances) > count:
+            account = ordered_accounts.pop()
+            base_account = account.split(":", 1)[0]
+            if base_account == self.ledger.options["name_expenses"]:
+                base_account = ":" + gettext("Other Expenses")
+            elif base_account == self.ledger.options["name_income"]:
+                base_account = ":" + gettext("Other Expenses")
+            else:
+                base_account = ":" + gettext("Other")
+            if base_account not in consolidated_balances:
+                consolidated_balances[base_account] = Inventory()
+            consolidated_balances[base_account].add_inventory(
+                account_balances[account]
+            )
+            del account_balances[account]
+        account_balances.update(consolidated_balances)
+
     @listify
     def interval_totals(
         self,
@@ -136,6 +169,7 @@ class ChartModule(FavaModule):
         accounts: str | tuple[str],
         conversion: str,
         invert: bool = False,
+        children: int = 0,
     ) -> Generator[DateAndBalanceWithBudget, None, None]:
         """Renders totals for account (or accounts) in the intervals.
 
@@ -143,19 +177,36 @@ class ChartModule(FavaModule):
             interval: An interval.
             accounts: A single account (str) or a tuple of accounts.
             conversion: The conversion to use.
+            children: number of categories to breakdown component accounts into
         """
+        # pylint: disable=too-many-locals
         price_map = self.ledger.price_map
         for begin, end in pairwise(self.ledger.interval_ends(interval)):
             inventory = Inventory()
             entries = iter_entry_dates(self.ledger.entries, begin, end)
+            account_inventories = {}
             for entry in (e for e in entries if isinstance(e, Transaction)):
                 for posting in entry.postings:
                     if posting.account.startswith(accounts):
+                        if posting.account not in account_inventories:
+                            account_inventories[posting.account] = Inventory()
+                        account_inventories[posting.account].add_position(
+                            posting
+                        )
                         inventory.add_position(posting)
-
             balance = cost_or_value(
                 inventory, conversion, price_map, end - ONE_DAY
             )
+            account_balances = {}
+            if children > 1:
+                for account, acct_value in account_inventories.items():
+                    account_balances[account] = cost_or_value(
+                        acct_value,
+                        conversion,
+                        price_map,
+                        end - ONE_DAY,
+                    )
+                self._consolidate_accounts(account_balances, children)
             budgets = {}
             if isinstance(accounts, str):
                 budgets = self.ledger.budgets.calculate_children(
@@ -166,10 +217,12 @@ class ChartModule(FavaModule):
                 # pylint: disable=invalid-unary-operand-type
                 balance = -balance
                 budgets = {k: -v for k, v in budgets.items()}
+                account_balances = {k: -v for k, v in account_balances.items()}
 
             yield DateAndBalanceWithBudget(
                 begin,
                 balance,
+                account_balances,
                 budgets,
             )
 
