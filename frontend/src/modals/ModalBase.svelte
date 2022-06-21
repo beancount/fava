1f353eebace987ae7f4dd913412b4b84166a7d3e
Lukas Dietrich <lukas@lukasdietrich.com>
2022-06-21 22:15:26 +0200

    Fix modal size on mobile

--- frontend/src/modals/ModalBase.svelte
+++ frontend/src/modals/ModalBase.svelte
@@ -55,6 +55,8 @@
 <style>
   .background {
     position: fixed;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
     cursor: pointer;
@@ -64,9 +66,9 @@
   .overlay {
     position: fixed;
     top: 0;
-    right: 0;
-    bottom: 0;
     left: 0;
+    width: 100vw;
+    height: 100vh;
     z-index: var(--z-index-overlay);
     display: flex;
     align-items: center;
