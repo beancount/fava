3ed4d2e4bd3ad8013d4b2d0d71f460541febb97e
Jakob Schnitzer <mail@jakobschnitzer.de>
2023-07-08 21:57:12 +0200

    draft: add support for uromyces

--- src/fava/beans/load.py
+++ src/fava/beans/load.py
@@ -4,7 +4,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import uromyces
 from beancount import loader
+from uromyces._convert import convert_options
 
 if TYPE_CHECKING:  # pragma: no cover
     from fava.beans.types import LoaderResult
@@ -19,11 +21,19 @@ def load_uncached(
     beancount_file_path: str,
     *,
     is_encrypted: bool,
+    use_uromyces: bool,
 ) -> LoaderResult:
     """Load a Beancount file."""
     if is_encrypted:  # pragma: no cover
         return loader.load_file(beancount_file_path)  # type: ignore[return-value]
 
+    if use_uromyces:
+        ledger = uromyces.load_file(beancount_file_path)
+        return (  # type: ignore[return-value]
+            ledger.entries,
+            ledger.errors,
+            convert_options(ledger),
+        )
     return loader._load(  # type: ignore[return-value]  # noqa: SLF001
         [(beancount_file_path, True)],
         None,
