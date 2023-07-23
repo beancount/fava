3381d928838eb27979b28a4ec93cf25de6b2e745
Jakob Schnitzer <mail@jakobschnitzer.de>
2023-07-23 15:40:49 +0200

    clipboard: use standard web APIs to copy to clipboard

--- frontend/src/clipboard.ts
+++ frontend/src/clipboard.ts
@@ -1,35 +1,14 @@
 import { log_error } from "./log";
 
-/**
- * Copy the given text to the clipboard.
- */
-function copyToClipboard(text: string | null): void {
-  if (!text) {
-    return;
-  }
-  const textarea = document.createElement("textarea");
-  textarea.value = text;
-  textarea.style.position = "fixed";
-  textarea.style.top = "0";
-  textarea.style.left = "0";
-  document.body.appendChild(textarea);
-  textarea.focus();
-  textarea.select();
-
-  try {
-    document.execCommand("copy");
-  } catch (err) {
-    log_error(err);
-  }
-  textarea.remove();
-}
-
 export class CopyableText extends HTMLElement {
   constructor() {
     super();
 
     this.addEventListener("click", (event) => {
-      copyToClipboard(this.getAttribute("data-clipboard-text"));
+      const text = this.getAttribute("data-clipboard-text");
+      if (text) {
+        navigator.clipboard.writeText(text).catch(log_error);
+      }
       event.stopPropagation();
     });
   }
