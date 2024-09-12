b71b1913396fe6cc56cf9bf8b845bf662f402871
Jakob Schnitzer <mail@jakobschnitzer.de>
2024-09-12 23:12:25 +0200

    add support for Beancount v3
    This uses beanquery to replace the functionality in beancount.query,
    which works with both Beancount v2 and v3 - however there are some
    differences on columns in beanquery compared to beancount.query.
    
    For importers, this sticks to beancount.ingest on v2 and used beangulp
    for v3. It still expectes importers to conform to the v2 importer
    protocol for now. Due to changes related to duplicate detection,
    duplicate detection is not automatically done by Fava here but can still
    be manually specified as hooks.

---
+++ src/fava/core/query.py
@@ -0,0 +1,167 @@
+"""Query result types."""
+
+from __future__ import annotations
+
+import datetime
+from dataclasses import dataclass
+from decimal import Decimal
+from typing import TYPE_CHECKING
+
+from beancount.core.amount import Amount
+from beancount.core.inventory import Inventory
+from beancount.core.position import Position
+
+from fava.core.conversion import simple_units
+
+if TYPE_CHECKING:  # pragma: no cover
+    from typing import Literal
+    from typing import TypeAlias
+    from typing import TypeVar
+
+    from fava.core.inventory import SimpleCounterInventory
+
+    T = TypeVar("T")
+
+    QueryRowValue = (
+        bool | int | str | datetime.date | Decimal | Position | Inventory
+    )
+    SerialisedQueryRowValue = (
+        bool
+        | int
+        | str
+        | datetime.date
+        | Decimal
+        | Position
+        | SimpleCounterInventory
+    )
+
+
+@dataclass(frozen=True)
+class QueryResultTable:
+    """Table query result."""
+
+    types: list[BaseColumn]
+    rows: list[tuple[SerialisedQueryRowValue, ...]]
+    t: Literal["table"] = "table"
+
+
+@dataclass(frozen=True)
+class QueryResultText:
+    """Text query result."""
+
+    contents: str
+    t: Literal["string"] = "string"
+
+
+if TYPE_CHECKING:  # pragma: no cover
+    QueryResult: TypeAlias = QueryResultTable | QueryResultText
+
+
+@dataclass(frozen=True)
+class BaseColumn:
+    """A query column."""
+
+    name: str
+    dtype: str
+
+    @staticmethod
+    def serialise(
+        val: QueryRowValue,
+    ) -> SerialisedQueryRowValue:
+        """Serialiseable version of the column value."""
+        return val  # type: ignore[return-value]
+
+
+@dataclass(frozen=True)
+class BoolColumn(BaseColumn):
+    """A boolean query column."""
+
+    dtype: str = "bool"
+
+
+@dataclass(frozen=True)
+class DecimalColumn(BaseColumn):
+    """A Decimal query column."""
+
+    dtype: str = "Decimal"
+
+
+@dataclass(frozen=True)
+class IntColumn(BaseColumn):
+    """A int query column."""
+
+    dtype: str = "int"
+
+
+@dataclass(frozen=True)
+class StrColumn(BaseColumn):
+    """A str query column."""
+
+    dtype: str = "str"
+
+
+@dataclass(frozen=True)
+class DateColumn(BaseColumn):
+    """A date query column."""
+
+    dtype: str = "date"
+
+
+@dataclass(frozen=True)
+class PositionColumn(BaseColumn):
+    """A Position query column."""
+
+    dtype: str = "Position"
+
+
+@dataclass(frozen=True)
+class SetColumn(BaseColumn):
+    """A set query column."""
+
+    dtype: str = "set"
+
+
+@dataclass(frozen=True)
+class AmountColumn(BaseColumn):
+    """An amount query column."""
+
+    dtype: str = "Amount"
+
+
+@dataclass(frozen=True)
+class ObjectColumn(BaseColumn):
+    """An object query column."""
+
+    dtype: str = "object"
+
+    @staticmethod
+    def serialise(val: object) -> str:
+        """Serialise an object of unknown type to a string."""
+        return str(val)
+
+
+@dataclass(frozen=True)
+class InventoryColumn(BaseColumn):
+    """A str query column."""
+
+    dtype: str = "Inventory"
+
+    @staticmethod
+    def serialise(  # type: ignore[override]
+        val: Inventory | None,
+    ) -> SimpleCounterInventory | None:
+        """Serialise an inventory."""
+        return simple_units(val) if val is not None else None
+
+
+COLUMNS = {
+    Amount: AmountColumn,
+    Decimal: DecimalColumn,
+    Inventory: InventoryColumn,
+    Position: PositionColumn,
+    bool: BoolColumn,
+    datetime.date: DateColumn,
+    int: IntColumn,
+    set: SetColumn,
+    str: StrColumn,
+}
