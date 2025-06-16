1ebeeeae56babf722fe60bc484954dc75a20f448
dtrai2 <d.trai282@gmail.com>
2025-06-16 10:46:48 +0200

    add test for narrations attribute

--- tests/test_core_attributes.py
+++ tests/test_core_attributes.py
@@ -72,3 +72,45 @@ def test_narration_transaction(example_ledger: FavaLedger) -> None:
     txn = attr.narration_transaction("Monthly bank fee")
     assert txn
     assert str(txn.date) == "2016-05-04"
+
+
+def test_narrations(example_ledger: FavaLedger) -> None:
+    attr = example_ledger.attributes
+    expected_narrations = [
+        "Investing 40% of cash in VBMPX",
+        "Investing 60% of cash in RGAGX",
+        "Payroll",
+        "Buying groceries",
+        "Eating out alone",
+        "Employer match for contribution",
+        "Eating out with Julie",
+        "Eating out ",
+        "Eating out after work",
+        "Eating out with Bill",
+        "Eating out with Natasha",
+        "Monthly bank fee",
+        "Paying off credit card",
+        "Eating out with Joe",
+        "Paying the rent",
+        "Tram tickets",
+        "Eating out with work buddies",
+        "Buy shares of VEA",
+        "Buy shares of GLD",
+        "Dividends on portfolio",
+        "Transfering accumulated savings to other account",
+        "Buy shares of ITOT",
+        "Buy shares of VHT",
+        "Consume vacation days",
+        "Sell shares of GLD",
+        "STATE TAX & FINANC PYMT",
+        "FEDERAL TAXPYMT",
+        "Allowed contributions for one year",
+        "Sell shares of VEA",
+        "Filing taxes for 2015",
+        "Sell shares of VHT",
+        "Sell shares of ITOT",
+        "Filing taxes for 2014",
+        "Opening Balance for checking account",
+        "Árvíztűrő tükörfúrógép",
+    ]
+    assert attr.narrations() == expected_narrations
