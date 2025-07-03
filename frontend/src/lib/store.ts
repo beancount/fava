83131eea3edb8fdefed575121e6a0126a8d3becb
Jakob Schnitzer <mail@jakobschnitzer.de>
2025-07-03 22:09:14 +0200

    store: listen to storage events; improve test coverage
    This syncs store changes between tabs (instead of loading the updated
    value on page reload).

--- frontend/src/lib/store.ts
+++ frontend/src/lib/store.ts
@@ -31,13 +31,18 @@ export function derived_array<S, T extends StrictEquality>(
 
 /** A store that has its value synced to localStorage. */
 export type LocalStoreSyncedStore<T> = Writable<T> & {
+  /** The key that this is stored under in localStorage. */
+  key: string;
   /** List all the values that this store can take. */
   values: () => [T, string][];
 };
 
+/** Keep track of all created stores. */
+const local_storage_synced_stores = new Set();
+
 /**
  * Create a store that syncs its value to localStorage.
- * @param key - The key to save this with in localStorage.
+ * @param key - The key to save this with in localStorage (will be prefixed with `fava-`).
  * @param validator - A Validator to check the loaded value.
  * @param init - A default to initialise the store with if localStorage is empty.
  * @param values - An optional enumerator of all possible values and descriptions.
@@ -48,25 +53,60 @@ export function localStorageSyncedStore<T>(
   init: () => T,
   values: () => [T, string][] = () => [],
 ): LocalStoreSyncedStore<T> {
-  const fullKey = `fava-${key}`;
+  if (key.startsWith("fava")) {
+    throw new Error("INTERNAL: should be called without 'fava-' prefix.");
+  }
+  const full_key = `fava-${key}`;
+  if (local_storage_synced_stores.has(full_key)) {
+    throw new Error(`INTERNAL: duplicate store with key '${key}'.`);
+  }
+  local_storage_synced_stores.add(full_key);
 
   // Create a store which is empty first but reads the value from
   // localStorage on the first subscription.
-  const store = writable<T>(undefined, (set) => {
-    const stored_val = localStorage.getItem(fullKey);
-    let initial: T | null = null;
-    if (stored_val != null) {
-      const val = parseJSON(stored_val).and_then(validator).unwrap_or(null);
-      if (val !== null) {
-        initial = val;
+  const {
+    set: store_set,
+    update: store_update,
+    subscribe,
+  } = writable<T>(undefined, (set) => {
+    const set_from_stored_value = (stored: string | null) => {
+      let initial: T | null = null;
+      if (stored != null) {
+        const res = parseJSON(stored).and_then(validator);
+        if (res.is_ok) {
+          initial = res.value;
+        }
       }
-    }
-    set(initial ?? init());
+      set(initial ?? init());
+    };
+    set_from_stored_value(localStorage.getItem(full_key));
 
-    store.subscribe((val) => {
-      localStorage.setItem(fullKey, JSON.stringify(val));
-    });
+    const listener = (event: StorageEvent) => {
+      if (event.storageArea === localStorage && event.key === full_key) {
+        set_from_stored_value(event.newValue);
+      }
+    };
+
+    window.addEventListener("storage", listener);
+    return () => {
+      window.removeEventListener("storage", listener);
+    };
   });
 
-  return { ...store, values };
+  return {
+    set: (val: T) => {
+      localStorage.setItem(full_key, JSON.stringify(val));
+      store_set(val);
+    },
+    update: (updater) => {
+      store_update((old_val) => {
+        const val = updater(old_val);
+        localStorage.setItem(full_key, JSON.stringify(val));
+        return val;
+      });
+    },
+    subscribe,
+    key: full_key,
+    values,
+  };
 }
