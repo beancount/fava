f8d30a2c7403211c7d820db20750387372460aef
Dimas Firmansyah <deirn@bai.lol>
2025-07-27 00:07:19 +0700

    style

--- frontend/src/journal/JournalTable.svelte
+++ frontend/src/journal/JournalTable.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { createVirtualizer } from "@tanstack/svelte-virtual";
-  import { onDestroy, onMount } from "svelte";
+  import { onDestroy } from "svelte";
   import { derived } from "svelte/store";
 
   import type { Entry } from "../entries";
@@ -22,86 +22,78 @@
 
   const { entries, showChangeAndBalance = false }: Props = $props();
 
-  let sortedEntries = $state.raw<Entry[]>([]);
-  let journalShow = derived(journalShowStore, (t) => new Set(t));
-
-  let head: HTMLLIElement;
-
-  const filter = derived(
-    [journalSortOrder, journalShow],
-    ([$order, $show]) =>
-      [$order, $show] as [JournalSortOrder, Set<JournalShowEntry>],
-  );
-
-  let unsub: () => void;
-  onMount(() => {
-    unsub = filter.subscribe(([journalSortOrder, journalShow]) => {
-      let column: SortColumn<Entry>;
-      switch (journalSortOrder[0]) {
-        case "flag":
-          column = new StringColumn("flag", (e) => e.sortFlag);
-          break;
-        case "narration":
-          column = new StringColumn("narration", (e) => e.sortNarration);
-          break;
-        default:
-          column = new DateColumn("date");
-          break;
+  let head = $state<HTMLLIElement>();
+  $effect(() => {
+    const order = $journalSortOrder;
+    head?.querySelectorAll<HTMLSpanElement>("span[data-sort]").forEach((el) => {
+      el.removeAttribute("data-order");
+      if (el.getAttribute("data-sort-name") === order[0]) {
+        el.setAttribute("data-order", order[1] ?? "asc");
       }
-      const sorter = new Sorter(column, journalSortOrder[1] ?? "asc");
+    });
+  });
 
-      const headers = head.querySelectorAll<HTMLSpanElement>("span[data-sort]");
-      headers.forEach((el) => {
-        el.removeAttribute("data-order");
-        if (el.getAttribute("data-sort-name") === column.name) {
-          el.setAttribute("data-order", sorter.order);
-        }
-      });
+  let sortedEntries = $state.raw<Entry[]>([]);
+  const journalShow = derived(journalShowStore, (t) => new Set(t));
+  const filter = derived([journalSortOrder, journalShow], (it) => it);
+  const unsub = filter.subscribe(([journalSortOrder, journalShow]) => {
+    let column: SortColumn<Entry>;
+    switch (journalSortOrder[0]) {
+      case "flag":
+        column = new StringColumn("flag", (e) => e.sortFlag);
+        break;
+      case "narration":
+        column = new StringColumn("narration", (e) => e.sortNarration);
+        break;
+      default:
+        column = new DateColumn("date");
+        break;
+    }
+    const sorter = new Sorter(column, journalSortOrder[1] ?? "asc");
 
-      // TODO: remove logging
-      console.time("filter");
-      const filtered = entries.filter((e) => {
-        if (journalShow.has(e.t.toLowerCase() as JournalShowEntry)) {
-          if (e.t === "Transaction") {
-            let flagOpt: JournalShowEntry;
-            switch (e.flag) {
-              case "*":
-                flagOpt = "cleared";
-                break;
-              case "!":
-                flagOpt = "pending";
-                break;
-              default:
-                flagOpt = "other";
+    // TODO: remove logging
+    console.time("filter");
+    const filtered = entries.filter((e) => {
+      if (journalShow.has(e.t.toLowerCase() as JournalShowEntry)) {
+        if (e.t === "Transaction") {
+          let flagOpt: JournalShowEntry;
+          switch (e.flag) {
+            case "*":
+              flagOpt = "cleared";
+              break;
+            case "!":
+              flagOpt = "pending";
+              break;
+            default:
+              flagOpt = "other";
+          }
+          return journalShow.has(flagOpt);
+        } else if (e.t === "Document") {
+          if (e.tags) {
+            if (e.tags.includes("discovered")) {
+              return journalShow.has("discovered");
             }
-            return journalShow.has(flagOpt);
-          } else if (e.t === "Document") {
-            if (e.tags) {
-              if (e.tags.includes("discovered")) {
-                return journalShow.has("discovered");
-              }
-              if (e.tags.includes("linked")) {
-                return journalShow.has("linked");
-              }
-            }
-          } else if (e.t === "Custom") {
-            if (e.type === "budget") {
-              return journalShow.has("budget");
+            if (e.tags.includes("linked")) {
+              return journalShow.has("linked");
             }
           }
-          return true;
+        } else if (e.t === "Custom") {
+          if (e.type === "budget") {
+            return journalShow.has("budget");
+          }
         }
-        return false;
-      });
-      console.timeEnd("filter");
-
-      // TODO: remove logging
-      console.time("sort");
-      const sorted = sorter.sort(filtered);
-      console.timeEnd("sort");
-
-      sortedEntries = sorted as Entry[];
+        return true;
+      }
+      return false;
     });
+    console.timeEnd("filter");
+
+    // TODO: remove logging
+    console.time("sort");
+    const sorted = sorter.sort(filtered);
+    console.timeEnd("sort");
+
+    sortedEntries = sorted as Entry[];
   });
 
   onDestroy(() => {
@@ -115,8 +107,8 @@
     $journalSortOrder = [name as JournalSortOrder[0], order];
   }
 
-  let ol = $state<HTMLDivElement>();
-  let lis = $state<HTMLLIElement[]>([]);
+  let vlistOuter = $state<HTMLDivElement>();
+  let vlistItems = $state<HTMLLIElement[]>([]);
 
   function depend<T, R>(t: T, fn: (t: T) => R) {
     return fn(t);
@@ -132,12 +124,12 @@
   let virtualizer = $derived(
     createVirtualizer({
       overscan: 5,
-      count: sortedEntries.length,
+      count: sortedEntries.length + 1,
       getItemKey: depend(
         sortedEntries,
         (sortedEntries) => (i) => sortedEntries[i]?.entry_hash ?? i,
       ),
-      getScrollElement: depend(ol, (ol) => () => ol ?? null),
+      getScrollElement: depend(vlistOuter, (ol) => () => ol ?? null),
       estimateSize: () => 50,
     }),
   );
@@ -145,86 +137,99 @@
   let items = $derived($virtualizer.getVirtualItems());
 
   $effect(() => {
-    if (lis.length) {
-      lis.forEach((li) => {
+    if (vlistItems.length) {
+      vlistItems.forEach((li) => {
         $virtualizer.measureElement(li);
       });
     }
   });
 </script>
 
-<JournalFilters />
-
-<ol class="flex-table journal">
-  <li class="head" bind:this={head}>
-    <p>
-      <!-- TODO: ARIA tags -->
-      <span
-        class="datecell"
-        data-sort="date"
-        data-sort-name="date"
-        onclick={headerClick}
-        aria-hidden="true"
-      >
-        {_("Date")}
-      </span>
-      <span
-        class="flag"
-        data-sort="string"
-        data-sort-name="flag"
-        onclick={headerClick}
-        aria-hidden="true"
-      >
-        {_("F")}
-      </span>
-      <span
-        class="description"
-        data-sort="string"
-        data-sort-name="narration"
-        onclick={headerClick}
-        aria-hidden="true"
-      >
-        {_("Payee")}/{_("Narration")}
-      </span>
-      <span class="num">{_("Units")}</span>
-      <span class="cost num">
-        {_("Cost")}
-        {#if showChangeAndBalance}
-          / {_("Change")}
-        {/if}
-      </span>
-      <span class="num">
-        {_("Price")}
-        {#if showChangeAndBalance}
-          / {_("Balance")}
-        {/if}
-      </span>
-    </p>
-  </li>
-
-  <div bind:this={ol} class="vlist-outer">
-    <div class="vlist-inner" style="height: {$virtualizer.getTotalSize()}px;">
-      <div
+<div class="fixed-fullsize-container">
+  <div bind:this={vlistOuter} class="vlist-outer">
+    <div
+      class="flex-table journal vlist-inner"
+      style="height: {$virtualizer.getTotalSize()}px;"
+    >
+      <ol
         class="vlist-items"
         style="transform: translateY({items[0]?.start ?? 0}px);"
       >
         {#each items as row (row.index)}
-          <JournalEntry
-            index={row.index}
-            e={getSortedEntry(row.index)}
-            {showChangeAndBalance}
-            journalShow={$journalShow}
-            bind:li={lis[row.index]}
-          />
+          {#if row.index === 0}
+            <li class="head" bind:this={head} data-index="0">
+              <div class="filter-container">
+                <JournalFilters />
+              </div>
+              <p>
+                <!-- TODO: ARIA tags -->
+                <span
+                  class="datecell"
+                  data-sort="date"
+                  data-sort-name="date"
+                  onclick={headerClick}
+                  aria-hidden="true"
+                >
+                  {_("Date")}
+                </span>
+                <span
+                  class="flag"
+                  data-sort="string"
+                  data-sort-name="flag"
+                  onclick={headerClick}
+                  aria-hidden="true"
+                >
+                  {_("F")}
+                </span>
+                <span
+                  class="description"
+                  data-sort="string"
+                  data-sort-name="narration"
+                  onclick={headerClick}
+                  aria-hidden="true"
+                >
+                  {_("Payee")}/{_("Narration")}
+                </span>
+                <span class="num">{_("Units")}</span>
+                <span class="cost num">
+                  {_("Cost")}
+                  {#if showChangeAndBalance}
+                    / {_("Change")}
+                  {/if}
+                </span>
+                <span class="num">
+                  {_("Price")}
+                  {#if showChangeAndBalance}
+                    / {_("Balance")}
+                  {/if}
+                </span>
+              </p>
+            </li>
+          {:else}
+            <JournalEntry
+              index={row.index}
+              e={getSortedEntry(row.index - 1)}
+              {showChangeAndBalance}
+              journalShow={$journalShow}
+              bind:li={vlistItems[row.index]}
+            />
+          {/if}
         {/each}
-      </div>
+      </ol>
     </div>
   </div>
-</ol>
+</div>
 
 <style>
+  .fixed-fullsize-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
   .vlist-outer {
-    height: 1000px;
+    height: 100%;
+    padding: 1.5em;
     contain: strict;
     overflow-y: auto;
   }
@@ -232,6 +237,7 @@
   .vlist-inner {
     position: relative;
     width: 100%;
+    margin: 0;
   }
 
   .vlist-items {
@@ -239,5 +245,9 @@
     top: 0;
     left: 0;
     width: 100%;
+  }
+
+  .filter-container {
+    margin-bottom: 0.25rem;
   }
 </style>
